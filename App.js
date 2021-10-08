f79dcb2f5aaa496175208c2d45e0ab8ef4648be5
Henrique Mariano <henrique_mariano@aluno.unb.br>
2021-10-08 18:32:38 -0300

    Adicionando comentário

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ import AppNavigate from "./components/AppNavigate";
  * Componente principal do aplicativo.
  */
 export default function App() {
+  /** Enquanto a fonte não carregar o aplicativo não é renderizado */
   let [fontsLoaded] = useFonts({
     Ruluko_400Regular,
   });
